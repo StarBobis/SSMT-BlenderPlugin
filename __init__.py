052c3e2dc0d6adef51662b50ae89627a67da89da
NicoMico <Administrator@DESKTOP>
2025-06-11 15:46:33 +0800

    12

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ bl_info = {
     "name": "SSMT",
     "description": "SSMT",
     "blender": (3, 6, 0),
-    "version": (1, 5, 7),
+    "version": (1, 5, 8),
     "location": "View3D",
     "category": "Generic"
 }
